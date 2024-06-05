8367a602f935b6ff1bfb634f9b5d57e7c668fe36
mksmbabak <100855091+mksmbabak@users.noreply.github.com>
2024-06-05 22:36:19 +0200

    Project

---
+++ L06_Example/Debug/Components/Src/bmp2.d
@@ -0,0 +1,5 @@
+Components/Src/bmp2.o: ../Components/Src/bmp2.c \
+ /Users/artemtymoshchuk/Documents/Study\ PUT/5\ semester/Microprocessor\ systems/Project/temp_lab/LAB6_5-6.zip_expanded/NUCLEO-F746ZG-Examples-ver-2023-24-fcf9b6ac044a2db760fdab9fb60b58e9200b54e7/L06_Example/Components/Inc/bmp2.h \
+ /Users/artemtymoshchuk/Documents/Study\ PUT/5\ semester/Microprocessor\ systems/Project/temp_lab/LAB6_5-6.zip_expanded/NUCLEO-F746ZG-Examples-ver-2023-24-fcf9b6ac044a2db760fdab9fb60b58e9200b54e7/L06_Example/Components/Inc/bmp2_defs.h
+/Users/artemtymoshchuk/Documents/Study\ PUT/5\ semester/Microprocessor\ systems/Project/temp_lab/LAB6_5-6.zip_expanded/NUCLEO-F746ZG-Examples-ver-2023-24-fcf9b6ac044a2db760fdab9fb60b58e9200b54e7/L06_Example/Components/Inc/bmp2.h:
+/Users/artemtymoshchuk/Documents/Study\ PUT/5\ semester/Microprocessor\ systems/Project/temp_lab/LAB6_5-6.zip_expanded/NUCLEO-F746ZG-Examples-ver-2023-24-fcf9b6ac044a2db760fdab9fb60b58e9200b54e7/L06_Example/Components/Inc/bmp2_defs.h:
